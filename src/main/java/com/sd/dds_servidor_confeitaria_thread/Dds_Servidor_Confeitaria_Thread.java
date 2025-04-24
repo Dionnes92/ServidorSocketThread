30771abbb5cba5e9604103a766194b4b24ab27a6
Dionnes <dionnes2323@gmail.com>
2025-04-24 15:07:13 -0300

    Criando projeto java servidor

---
+++ src/main/java/com/sd/dds_servidor_confeitaria_thread/Dds_Servidor_Confeitaria_Thread.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sd.dds_servidor_confeitaria_thread;
+
+/**
+ *
+ * @author Dionnes
+ */
+public class Dds_Servidor_Confeitaria_Thread {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
