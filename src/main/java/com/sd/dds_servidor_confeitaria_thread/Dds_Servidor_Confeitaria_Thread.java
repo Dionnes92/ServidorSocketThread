3cbebb6ad8e6e1b00194ea4768a5fcd91050a5cd
Dionnes <dionnes2323@gmail.com>
2025-04-24 15:24:09 -0300

    Criacao do formulario

--- src/main/java/com/sd/dds_servidor_confeitaria_thread/Dds_Servidor_Confeitaria_Thread.java
+++ src/main/java/com/sd/dds_servidor_confeitaria_thread/Dds_Servidor_Confeitaria_Thread.java
@@ -4,13 +4,19 @@
 
 package com.sd.dds_servidor_confeitaria_thread;
 
+import java.net.Socket;
+import java.util.Vector;
+
 /**
  *
  * @author Dionnes
  */
-public class Dds_Servidor_Confeitaria_Thread {
+public class Dds_Servidor_Confeitaria_Thread extends Thread {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       produtos = new Vector();
     }
+    private static Vector produtos;
+    private Socket conexao;
+    
 }
