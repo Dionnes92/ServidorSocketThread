7cbd1f6a2c9f3228294408ff4c77cf542f0c9687
Dionnes92 <dionnes2323@gmail.com>
2025-05-01 19:13:50 -0300

    Ajuste do arquivo escrever txt

--- src/main/java/com/sd/dds_servidor_confeitaria_thread/Dds_ManipularArquivo.java
+++ src/main/java/com/sd/dds_servidor_confeitaria_thread/Dds_ManipularArquivo.java
@@ -16,43 +16,47 @@ import java.io.PrintWriter;
  * @author Dionnes
  */
 public class Dds_ManipularArquivo {
-    
-    
-       public void Dds_escreverArq(String Dds_texto, String Dds_Bolos, boolean Dds_manter) 
-    { 
-        try 
+
+    public void Dds_escreverArq(String Dds_texto, String Dds_Bolos, boolean Dds_manter) {
+        
+         try 
         { 
-            File Dds_f = new File("c:\\"+Dds_Bolos+".txt"); 
+            
+            File Dds_f = new File("C:\\Dds_Bolos.txt"); 
  
             FileWriter Dds_fw = new FileWriter(Dds_f,Dds_manter); 
-            PrintWriter pw = new PrintWriter(Dds_fw); 
-            pw.println(Dds_texto); 
-            Dds_fw.close(); 
+            PrintWriter Dds_pw = new PrintWriter(Dds_fw); 
+            Dds_pw.println(Dds_texto); 
+            Dds_pw.close(); 
         } 
         catch(IOException ex) 
         { 
             ex.printStackTrace(); 
         } 
-    } 
-    public BufferedReader Dds_lerArq(String Dds_nomeArq) 
-    { 
-        BufferedReader br = null; 
-        try 
-        { 
-            File Dds_f = new File("c:\\"+Dds_nomeArq+".txt"); 
-            //System.out.println(f.exists()); 
-            if(Dds_f.exists()) 
-            { 
-                FileReader Dds_fr = new FileReader(Dds_f); 
-                br = new BufferedReader(Dds_fr); 
-            } 
-        } 
-        catch(IOException ex) 
-        { 
-            ex.printStackTrace(); 
-        } 
-         
-        return br; 
+
+       
     }
-    
+
+    public BufferedReader Dds_lerArq(String Dds_Bolos){
+        BufferedReader br = null;
+        try {
+            File Dds_f = new File("c:\\objetoT"+Dds_Bolos+".txt");
+            //System.out.println(f.exists()); 
+            if (Dds_f.exists()) {
+                FileReader Dds_fr = new FileReader(Dds_f);
+                br = new BufferedReader(Dds_fr);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return br;
+        
+         
+         
+    }
+      
+
 }
+
+    
